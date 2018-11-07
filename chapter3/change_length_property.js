c18007c08eeb41a60ad847dfe58e07d44475f693
2Fe <hee861213@naver.com>
2018-11-07 14:38:56 +0900

    배열의 length property 는 배열의 실재 길이를 가저오는 것이 아니라 배열의 가장 큰 index에 +1을 하여 출력

---
+++ chapter3/change_length_property.js
@@ -0,0 +1,10 @@
+var arr = [];
+console.log(arr.length);        // (출력값) 0
+
+arr[0] = 0;     // arr.length = 1
+arr[0] = 1;     // arr.length = 2
+arr[0] = 2;     // arr.length = 3
+arr[100] = 100;
+
+console.log(arr.length);    // (출력값) 101
+
