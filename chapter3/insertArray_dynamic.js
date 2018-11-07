d8f46a5158822a61e02f980cc4a9f85c3219fc11
2Fe <hee861213@naver.com>
2018-11-07 14:09:20 +0900

    array 에 동적으로 원소를 추가하는 예제

---
+++ chapter3/insertArray_dynamic.js
@@ -0,0 +1,10 @@
+// 빈 배열
+var emptyArr =[];
+console.log(emptyArr[0]);       // (출력값) undefined
+
+// 배열 요소 동적 생성
+emptyArr[0] = 100;
+emptyArr[3] = 'eight';
+emptyArr[7] = true;
+console.log(emptyArr);          // (출력값) [100, undefined, undefined, "eight", undefined, undefined, undefined, true]
+console.log(emptyArr.length);      // (출력값) 8
