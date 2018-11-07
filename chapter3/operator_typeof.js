9ccabe8649c77d52d198da8c95ddd9db9db80b12
2Fe <hee861213@naver.com>
2018-11-07 18:01:24 +0900

    javascript 의 자료형 타입

---
+++ chapter3/operator_typeof.js
@@ -0,0 +1,13 @@
+/*
+각 타입 별 typeof 연산자 결과
+
+기본타입            숫자            'number'
+기본타입            문자열          'string'
+기본타입            boolean         'boolean'
+기본타입            null            'object'
+기본타입            undefined       'undefined'
+참조타입            객체            'object'
+참조타입            배열            'object'
+참조타입            함수            'function'
+
+*/
