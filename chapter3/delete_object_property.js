f206115953b389c573d23102a53a04a7b7523699
2Fe <hee861213@naver.com>
2018-11-07 11:42:15 +0900

    객체의 프로퍼티 삭제 예제

---
+++ chapter3/delete_object_property.js
@@ -0,0 +1,14 @@
+// 객체 리터럴을 이용한 foo 객체 생성
+var foo = {
+    name:'foo',
+    nickname: 'babo'
+};
+
+console.log(foo.nickname);      //  (출력값) babo
+delete foo.nickname;            //  (출력값) nickname 프로퍼티 삭제
+console.log(foo.nickname);      //  (출력값) undefined
+
+delete foo;                     //  (출력값) foo 객체 삭제 시도
+console.log(foo.name);          //  (출력값) foo
+
+
