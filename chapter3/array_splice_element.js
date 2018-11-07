4548ffe7ed7950011a17bf9328f853745119698c
2Fe <hee861213@naver.com>
2018-11-07 16:56:19 +0900

    splice() 메서드를 이용하여 배열의 프로퍼티를 완전시 삭제

---
+++ chapter3/array_splice_element.js
@@ -0,0 +1,7 @@
+var arr = ['zero', 'one', 'two', 'three'];
+
+arr.splice(2, 1);            // 2 번째 요소를 시작점으로 1개의 요소를 삭제한다.
+
+console.log(arr);           // (출력값) ["zero", "one", "three"]
+console.log(arr.length);    // (출력값) 3
+
