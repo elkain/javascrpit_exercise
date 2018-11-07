9f7e0c81418e741d96c58a9a5a498808d8561bff
2Fe <hee861213@naver.com>
2018-11-07 15:05:52 +0900

    배열의 length 를 명시적으로 변환해주는 예제

---
+++ chapter3/change_length_property_explicit.js
@@ -0,0 +1,9 @@
+var arr = [0, 1, 2];
+console.log(arr.length);    // (출력값) 3
+
+arr.length = 5;
+console.log(arr);           // (출력값) [0, 1, 2, undefined, undefined]
+
+arr.length = 2;
+console.log(arr);           // (출력값) [0, 1]
+console.log(arr[2]);        // (출력값) undefined
