5e051164bad055c2f9454f659b475f88642e3756
2Fe <hee861213@naver.com>
2018-11-08 17:26:01 +0900

    내부 함수에서 this 사용시 전역 객체(window)로 바인딩 됨을 보여주는 예제

---
+++ chapter4/thisBinding_innerFunctionCall.js
@@ -0,0 +1,29 @@
+// 내부함수의 this 바인딩 동작을 보여주는 예제 코드
+var value = 100;
+
+// myObject 객체 생성
+var myObject = {
+    value:1,
+    func1 : function(){
+        this.value += 1;
+        console.log('func1() called. this.value :'+this.value);
+        
+        // func2() 내부 함수
+        func2 = function(){
+            this.value += 1;
+            console.log('func2() called. this.value : '+this.value);
+            
+            // func3() 내부함수
+            func3 = function(){
+                this.value += 1;
+                console.log('func3() called. this.value : ' + this.value);
+            }
+
+            func3();        // func3() 내부 함수 호출
+        }
+
+        func2();            // func2() 내부 함수 호출
+    }
+};
+
+myObject.func1();           // func1() 메서드 호출
