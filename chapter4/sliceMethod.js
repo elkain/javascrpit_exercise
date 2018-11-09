03394bb7c776aad644a4eee97386b8a997288010
2Fe <hee861213@naver.com>
2018-11-09 10:09:47 +0900

    slice 메서드 활용 예제

---
+++ chapter4/sliceMethod.js
@@ -0,0 +1,7 @@
+// slice() 메서드 사용예제
+
+var arrA = [1, 2, 3];
+var arrB = arrA.slice(0);       // [1, 2, 3]
+var arrC = arrA.slice();        // [1, 2, 3]
+var arrD = arrA.slice(1);       // [2, 3]
+var arrE = arrA.slice(1,2);     // [2]
