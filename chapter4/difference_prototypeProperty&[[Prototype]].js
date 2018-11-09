125e886d19cb8724ef8dcb741937377e8d43c30f
2Fe <hee861213@naver.com>
2018-11-09 11:48:22 +0900

    prototype property 와 __proto__ 의 차이 및 생성 시 내부 구조 비교 예제

---
+++ chapter4/difference_prototypeProperty&[[Prototype]].js
@@ -0,0 +1,12 @@
+// prototype Property 와 [[Prototype]] 링크 구분
+
+// Person 생성자 함수
+function Person(name){
+    this.name = name;
+}
+
+// foo 객체 생성
+var foo = new Person('foo');
+
+console.dir(Person);
+console.dir(foo);
